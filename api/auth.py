2b514a40b780e109191c677f86f47628f84e21d5
suhailvs <suhailvs@gmail.com>
2026-03-05 17:28:25 +0530

    fixed bug

--- api/auth.py
+++ api/auth.py
@@ -4,18 +4,19 @@ from functools import wraps
 import time
 import json
 from rest_framework.response import Response
+from django.conf import settings
 from .models import Node
 from .utils import verify_signature, generate_signature
 
 def consensus_required(view_func):
     @wraps(view_func)
     def wrapped_view(request, *args, **kwargs):
-        public_key = request.headers.get("X-Public-Key")
+        node_id = request.headers.get("X-Node-Id")
         signature = request.headers.get("X-Signature")
         timestamp = request.headers.get("X-Timestamp")
-
-        if not Node.objects.filter(public_key=public_key):
-            return Response({"error": "Unauthorized: Given public key not in TRUSTED_PEERS"}, status=401)
+        node = Node.objects.filter(node_id=node_id).first()
+        if not node:
+            return Response({"error": "Unauthorized: Given node_id not in TRUSTED_PEERS"}, status=401)
         
         if abs(time.time() - int(timestamp)) > 5: # replay protection (5 seconds window)
             return Response({"error": "Unauthorized: Replay attack detected"}, status=401)
@@ -23,7 +24,7 @@ def consensus_required(view_func):
         body = request.body.decode()
         message = f"{request.method}|{request.path}|{body}|{timestamp}"
 
-        if not verify_signature(public_key,signature,message):
+        if not verify_signature(node.public_key,signature,message):
             return Response({"error": "Unauthorized: Signature not match."}, status=401)
         return view_func(request, *args, **kwargs)
     return wrapped_view
@@ -33,7 +34,7 @@ def create_consensus_auth_headers(method, path, body):
     body_json = json.dumps(body or {}, separators=(",", ":"), sort_keys=True)
     message = f"{method.upper()}|{path}|{body_json}|{timestamp}"
     return {
-        "X-Public-Key": NODE_PUBLIC_KEY,
+        "X-Node-Id": settings.LOCAL_NODE_ID,
         "X-Signature": generate_signature(message),
         "X-Timestamp": timestamp,
         "Content-Type": "application/json",
